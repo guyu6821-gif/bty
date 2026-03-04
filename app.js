a06d1ce4de50a1c2eaf46d0c65473ad042b4bca3
guyu6821-gif <guyu6821@gmail.com>
2026-03-04 14:26:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -551,7 +551,7 @@ const akademikMelumatlar = [
     "Kəsr fənni olan tələbə həmin fənnin kredit sayına uyğun olaraq, istəyə bağlı şəkildə 25% ödəniş edib ikinci şans imtahanında iştirak edə bilər. 25% ödəniş bu düsturla hesablanır: [((illik ödəniş / 60) × kredit sayı) / 4] + 1.",
     "Əgər tələbə kəsilməzdən əvvəl təqaüd alırdısa, kəsildiyi halda təqaüdünü itirir və 25% ödənişlə imtahanı uğurla versə belə, təqaüd bərpa olunmur.",
     "ÜOMG (Ümumi Orta Müvəffəqiyyət Göstəricisi) tələbənin ümumi akademik nəticəsidir və universiteti bitirərkən gələcək təhsilinə mühüm təsir göstərir. ÜOMG 100 ballıq sistemlə bu düsturla hesablanır: (bal1 × kredit1 + bal2 × kredit2 + ... + bal n × kredit n) / (kredit1 + kredit2 + ... + kredit n).",
-    "Qayıb limiti: Hər fənn üzrə dərs saatına uyğun qayıb limiti müəyyən edilir. Bu limiti aşan tələbə imtahana buraxılmır və avtomatik olaraq kəsilmiş sayılır. Mühazirə və seminar üzrə qayıb sayı ortaq hesablanır."
+    "Qayıb limiti: Hər fənn üzrə dərs saatına uyğun qayıb limiti müəyyən edilir. Bu limiti aşan tələbə imtahana buraxılmır və avtomatik olaraq kəsilmiş sayılır. Mühazirə və seminar üzrə qayıb sayı ortaq hesablanır.",
     "91–100 bal A ilə qiymətləndirilir və əla qiymət sayılır; 81–90 bal B ilə qiymətləndirilir və çox yaxşı hesab olunur; 71–80 bal C ilə qiymətləndirilir və yaxşı qiymət sayılır; 61–70 bal D ilə qiymətləndirilir və kafi hesab olunur; 51–60 bal E ilə qiymətləndirilir və qaneedici hesab olunur; 51-dən aşağı bal F ilə qiymətləndirilir və qeyri-kafi hesab olunur.",
     // Yeni məlumatlar əlavə etmək üçün dırnaq içində yaz:
     // "Yeni məlumat buraya",
