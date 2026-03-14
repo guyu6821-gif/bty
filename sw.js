614e60a934c78e57879f9a2509fbba7ab562e4e7
guyu6821-gif <guyu6821@gmail.com>
2026-03-14 17:27:55 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Version - Cache yeniləmə üçün bu nömrəni artır
-const CACHE_VERSION = 'v1.3.16';
+const CACHE_VERSION = 'v1.3.17';
 const CACHE_NAME = `bdu-hesablayici-${CACHE_VERSION}`;
 
 // iOS Safari üçün sadələşdirilmiş cache siyahısı
